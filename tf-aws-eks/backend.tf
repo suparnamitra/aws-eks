30cba5392d10996bc0a6fab563e7c8a6002bbc4c
Vijay Vishwakarma <sharmavijay86@gmail.com>
2025-03-18 10:09:24 +0530

    backend

--- tf-aws-eks/backend.tf
+++ tf-aws-eks/backend.tf
@@ -1,18 +1,4 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.90.1"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.0"
-    }
-    kubernetes = {
-      source = "hashicorp/kubernetes"
-      version = "2.36.0"
-    }
-  }
   backend "s3" {
     bucket = "tcslabsfjbs"
     key    = "eks/terraform.tfstate"
